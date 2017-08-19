835636cb77a05c9ffad3eae40f979b288c790f1c
Bert Lorenz <bertlorenz@gmail.com>
2017-08-19 12:19:36 +0200

    remove gnucash temporarily

--- startup.sh
+++ startup.sh
@@ -4,5 +4,4 @@ Xvfb :1 -screen 0 1440x900x16 &
 sleep 5
 openbox-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-gnucash --display=:1 &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
+ln -s /root/noVNC/vnc_auto.html /root/noVNC/index.html && /root/noVNC/utils/launch.sh --vnc localhost:5900
