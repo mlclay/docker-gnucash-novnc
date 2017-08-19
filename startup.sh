cb4edace7cc603508ec221249c1b4740a15dff20
Bert Lorenz <bertlorenz@gmail.com>
2017-08-19 12:42:05 +0200

    creating variable for novnc path

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export DISPLAY=:1
+export NOVNC=/root/noVNC
 Xvfb :1 -screen 0 1440x900x16 &
 sleep 5
 openbox-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-ln -s /root/noVNC/vnc.html /root/noVNC/index.html && /root/noVNC/utils/launch.sh --vnc localhost:5900
+ln -s $NOVNC/vnc.html $NOVNC/index.html && $NOVNC/utils/launch.sh --vnc localhost:5900
