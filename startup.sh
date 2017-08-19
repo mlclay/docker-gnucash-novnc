ea9cebef4a7a687fcf6d0fc309f686f1d6f3d6e9
Bert Lorenz <bertlorenz@gmail.com>
2017-08-19 12:30:40 +0200

    route index.html to vnc.html
    vnc_auto.html does not exist.

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ Xvfb :1 -screen 0 1440x900x16 &
 sleep 5
 openbox-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-ln -s /root/noVNC/vnc_auto.html /root/noVNC/index.html && /root/noVNC/utils/launch.sh --vnc localhost:5900
+ln -s /root/noVNC/vnc.html /root/noVNC/index.html && /root/noVNC/utils/launch.sh --vnc localhost:5900
