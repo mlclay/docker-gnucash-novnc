278cd50a1dec460ca0e99ab128c65dc77f80ee8a
Bert Lorenz <bertlorenz@gmail.com>
2017-08-21 19:38:08 +0200

    autostart gnucash

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ Xvfb :1 -screen 0 1440x900x16 &
 sleep 5
 openbox-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-ln -s $NOVNC/vnc.html $NOVNC/index.html && $NOVNC/utils/launch.sh --vnc localhost:5900
+ln -s $NOVNC/vnc.html $NOVNC/index.html && $NOVNC/utils/launch.sh --vnc localhost:5900 &
+gnucash
