1c1a67feb38387340bc7096dff0fb08daccd6aae
Bert Lorenz <bertlorenz@gmail.com>
2017-08-23 09:46:27 +0200

    enable vnc password protection

--- README.md
+++ README.md
@@ -13,9 +13,11 @@ docker run -td \
 	-p 6080:6080 \
 	-e LOCALE=<your_LOCALE> \
 	-e FILE=<your_gnucash_file> \
+	-e VNC_PASS=<your_password> \
 	bertlorenz/gnucash-novnc:<tag>
 ```
-Then access via http://localhost:6080
+Then access via http://localhost:6080.
+Default Password is gnucash
 
 ### Locales
 The following locales are preinstalled
--- startup.sh
+++ startup.sh
@@ -13,12 +13,18 @@ then
 	export LC_ALL=$LOCALE
 fi
 
+#Define standard password
+if [[ -z  $VNC_PASS ]]
+then
+	export VNC_PASS=gnucash
+fi
+
 #Startup
 export DISPLAY=:1
 export NOVNC=/opt/noVNC
 Xvfb :1 -screen 0 1440x900x16 &
 sleep 5
 openbox-session&
-x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+x11vnc -display :1 -nopw -listen localhost -passwd $VNC_PASS -xkb -ncache 10 -ncache_cr -forever &
 ln -s $NOVNC/vnc.html $NOVNC/index.html && $NOVNC/utils/launch.sh --vnc localhost:5900 &
 gnucash $FILE
